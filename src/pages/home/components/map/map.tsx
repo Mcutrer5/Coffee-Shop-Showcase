706b04f948bd8cfd8b76eb05844d592f66dfcb57
Mcutrer5 <60704217+Mcutrer5@users.noreply.github.com>
2023-10-06 14:19:20 -0500

    Update map.tsx

--- src/pages/home/components/map/map.tsx
+++ src/pages/home/components/map/map.tsx
@@ -2,37 +2,37 @@ import React, { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 
 const Map: React.FC = () => {
-    return (<></>)    
-    // (mapboxgl as any).accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
-    // // (mapboxgl as any).accessToken = "";
+    
+    (mapboxgl as any).accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
+    // (mapboxgl as any).accessToken = "";
 
-    // const mapContainer = useRef<string | HTMLElement>("mapContainer");
-    // const map = useRef<mapboxgl.Map | null>(null);
+    const mapContainer = useRef<string | HTMLElement>("mapContainer");
+    const map = useRef<mapboxgl.Map | null>(null);
 
-    // const [viewport, setViewport] = React.useState({
-    //     latitude: 45.4211,
-    //     longitude: -75.6903,
-    //     zoom: 12,
-    // });
+    const [viewport, setViewport] = React.useState({
+        latitude: 45.4211,
+        longitude: -75.6903,
+        zoom: 12,
+    });
 
-    // useEffect(() => {
-    //     if (map.current) return; // initialize map only once
-    //     map.current = new mapboxgl.Map({
-    //         container: mapContainer.current,
-    //         style: "mapbox://styles/mapbox/streets-v12", // style URL
-    //         center: [viewport.longitude, viewport.latitude],
-    //         zoom: viewport.zoom,
-    //     });
-    //     map.current.on('load', function () {
-    //         map.current?.resize();
-    //     })
-    // }, []);
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: "mapbox://styles/mapbox/streets-v12", // style URL
+            center: [viewport.longitude, viewport.latitude],
+            zoom: viewport.zoom,
+        });
+        map.current.on('load', function () {
+            map.current?.resize();
+        })
+    }, []);
 
-    // return (
-    //     <div>
-    //         <div ref={(el) => (mapContainer.current = el ? el : "")} style={{ height: "100vh", width: "100%" }} className="mapContainer" />
-    //     </div>
-    // )
+    return (
+        <div>
+            <div ref={(el) => (mapContainer.current = el ? el : "")} style={{ height: "100vh", width: "100%" }} className="mapContainer" />
+        </div>
+    )
 }
 
 export default Map;
